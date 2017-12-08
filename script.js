f27bbd0204bfd1cd6f6a9a5ee976543659e99d56
Dominika <adamskdominik@gmail.com>
2017-12-08 02:31:43 +0100

    Sketch of the survey

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+
   
   var showNext = document.querySelectorAll('.nextQuestionBox');
   
   for (var i = 0; i < showNext.length; i++ ) {
     
-    showNext[i].addEventListener("click", function () {
+    showNext[i].addEventListener("click", function (event) {
+
+        event.preventDefault();
       //to działa
       this.parentElement.nextElementSibling.style.display="block";
       this.parentElement.style.display="none";
