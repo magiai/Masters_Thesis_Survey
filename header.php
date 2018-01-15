a862838ee3dc420cdb3fd04e59a564ee6514f42e
Dominika <adamskdominik@gmail.com>
2018-01-15 12:19:29 +0100

    Individual differences section

---
+++ header.php
@@ -0,0 +1,56 @@
+<div class="questionSwitchContainer subMainBox">
+    <div class="imagePosition"><div class = "image img1"></div></div>
+    <div>
+        <h3 class="question">Czy to zdjęcie Ci się podoba?</h3>
+        <ul class="likert">
+            <li class="likertElement">Zdecydowanie nie<input id="scaleStart" type="radio" name="estimation" value="1" />
+            <li class="likertElement">Nie<input type="radio" name="estimation" value="2" />
+            <li class="likertElement">Raczej nie<input type="radio" name="estimation" value="3" />
+            <li class="likertElement">Nie mam zdania<input type="radio" name="estimation" value="4" />
+            <li class="likertElement">Raczej tak<input type="radio" name="estimation" value="5"/>
+            <li class="likertElement">Tak<input type="radio" name="estimation" value="6" />
+            <li class="likertElement">Zdecydowanie tak<input id="scaleEnd" type="radio" name="estimation" value="7" />
+        </ul>
+    </div>
+    <div class="nextQuestionBox">
+        <button class="nextQuestion">Następne</button>
+    </div>
+</div>
+
+<div class="questionSwitchContainer subMainBox">
+    <div class="imagePosition"><div class = "image img2"></div></div>
+    <div>
+        <h3 class="question">Czy to zdjęcie Ci się podoba?</h3>
+        <ul class="likert">
+            <li class="likertElement">Zdecydowanie nie<input id="scaleStart" type="radio" name="estimation" value="1" />
+            <li class="likertElement">Nie<input type="radio" name="estimation" value="2" />
+            <li class="likertElement">Raczej nie<input type="radio" name="estimation" value="3" />
+            <li class="likertElement">Nie mam zdania<input type="radio" name="estimation" value="4" />
+            <li class="likertElement">Raczej tak<input type="radio" name="estimation" value="5"/>
+            <li class="likertElement">Tak<input type="radio" name="estimation" value="6" />
+            <li class="likertElement">Zdecydowanie tak<input id="scaleEnd" type="radio" name="estimation" value="7" />
+        </ul>
+    </div>
+    <div class="nextQuestionBox">
+        <button class="nextQuestion">Następne</button>
+    </div>
+</div>
+
+<div class="questionSwitchContainer subMainBox">
+    <div class="imagePosition"><div class = "image img3"></div></div>
+    <div>
+        <h3 class="question">Czy to zdjęcie Ci się podoba?</h3>
+        <ul class="likert">
+            <li class="likertElement">Zdecydowanie nie<input id="scaleStart" type="radio" name="estimation" value="1" />
+            <li class="likertElement">Nie<input type="radio" name="estimation" value="2" />
+            <li class="likertElement">Raczej nie<input type="radio" name="estimation" value="3" />
+            <li class="likertElement">Nie mam zdania<input type="radio" name="estimation" value="4" />
+            <li class="likertElement">Raczej tak<input type="radio" name="estimation" value="5"/>
+            <li class="likertElement">Tak<input type="radio" name="estimation" value="6" />
+            <li class="likertElement">Zdecydowanie tak<input id="scaleEnd" type="radio" name="estimation" value="7" />
+        </ul>
+    </div>
+    <div class="nextQuestionBox">
+        <button class="nextQuestion">Następne</button>
+    </div>
+</div>
---
+++ templates/likertScalePics.php
@@ -0,0 +1,12 @@
+<div>
+        <h3 class="question">Czy to zdjęcie Ci się podoba?</h3>
+        <ul class="likert">
+            <li class="likertElement">Jest brzydkie<input id="scaleStart" type="radio" name="estimation" value="1" />
+            <li class="likertElement">Nie<input type="radio" name="estimation" value="2" />
+            <li class="likertElement">Raczej nie<input type="radio" name="estimation" value="3" />
+            <li class="likertElement">Nie mam zdania<input type="radio" name="estimation" value="4" />
+            <li class="likertElement">Raczej tak<input type="radio" name="estimation" value="5"/>
+            <li class="likertElement">Tak<input type="radio" name="estimation" value="6" />
+            <li class="likertElement">Jest piękne<input id="scaleEnd" type="radio" name="estimation" value="7" />
+        </ul>
+</div>
